7d64231226435ea17625563e81dab05f82913360
Guilherme Cunha <guicunharib@gmail.com>
2025-09-05 14:37:04 -0300

    create getCommitsDiffs & getDependecies on repoinfo_functions

--- repoinfo_functions.py
+++ repoinfo_functions.py
@@ -1,6 +1,7 @@
 from git import Repo
 from datetime import datetime
 import os
+from collections import Counter
 
 # Variáveis que virão do .config
 repo_path = r"C:\Users\guicu\OneDrive\Documentos\prog\aise\GithubDocs" # caminho do repositório *OBRIGATÓRIO
@@ -38,5 +39,33 @@ def getCommits(branch="main", start_date=start_date, end_date=end_date, output_f
 
             f.write(f"{commit.message.strip()}\n")
     print(f"Commits salvos em {output_file}")
+    return output_file
 
+def getCommitDiffs(branch="main", start_date=start_date, end_date=end_date, output_filename="diffs.txt"):
+    output_file = os.path.join(output_dir, output_filename)
+
+    with open(output_file, "w", encoding="utf-8") as f:
+        commits = list(repo.iter_commits(branch))
+        for i in range(len(commits)-1):
+            commit = commits[i]
+            parent = commits[i+1]  # commit anterior
+
+            commit_date = commit.committed_datetime
+            if start_date and commit_date < start_date:
+                continue
+            if end_date and commit_date > end_date:
+                continue
+
+            diffs = commit.diff(parent, create_patch=True)
+            for diff in diffs:
+                f.write(diff.diff.decode('utf-8', errors='ignore') + "\n")
+
+    print(f"Diferenças salvas em {output_file}")
+    return output_file
+
+def getDependencies(dependency_file=dependency_file):
+    return os.path.join(repo_path, dependency_file)
+
+getDependencies()
+getCommitDiffs()
 getCommits()
