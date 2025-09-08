f3040c6f42c78502d631a0ebc0dd8130e9aac04f
Pedro Verardo <pedro.lopesverardo@gmail.com>
2025-09-08 06:12:47 -0300

    configuration data classes

--- src/load_configuration/conf_structures.py
+++ src/load_configuration/conf_structures.py
@@ -1,21 +1,105 @@
-
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List, Dict, Any, Optional
 
 @dataclass
-class LLMConfig:
+class LLMParameters:
+    temperature: float
+    max_tokens: int
+
+@dataclass
+class LLMConfiguration:
+    name: str
     model: str
     api_key: str
-    parameters: dict
+    parameters: LLMParameters
 
 @dataclass
-class TargetConfig:
-    repository_path: str
-    type: str
-    readme: dict
-    changelog: dict
+class LLMProvider:
+    family: str
+    configurations: List[LLMConfiguration]
+
+@dataclass
+class LLM:
+    providers: List[LLMProvider]
+    default_provider: str
+
+@dataclass
+class TargetInformation:
+    repo_path: str
+    branch_name: str
+    dependency_file_path: str
+    programing_language: str
+    expected_result_type: str
+    start_date: str = ""
+    end_date: str = ""
+    description: str = ""
+    framework: str = "none"
+
+@dataclass
+class ExtractInformation:
+    types: List[str]
+
+@dataclass
+class AgentComponent:
+    name: str
+    description: str
+    prompt: str
+    model_name: str
+
+@dataclass
+class Agents:
+    components: List[AgentComponent]
+
+@dataclass
+class OrchestrationFlowStep:
+    step: int
+    from_step: str 
+    to: str
+    extract_information_types: Optional[str] = None
+    
+    @classmethod
+    def from_dict(cls, data: Dict[str, Any]):
+        return cls(
+            step=data["step"],
+            from_step=data["from"],
+            to=data["to"],
+            extract_information_types=data.get("extract_information_types")
+        )
+
+@dataclass
+class Orchestration:
+    max_retries: int
+    timeout_seconds: int
+    flow: List[OrchestrationFlowStep]
+
+@dataclass
+class OutputFile:
+    name: str
+    format: str
+
+@dataclass
+class OutputComponent:
+    name: str
+    path: str
+    template: str
+    file: OutputFile
+
+@dataclass
+class Output:
+    components: List[OutputComponent]
 
 
 @dataclass
-class EvaluationConfig:
+class Evaluation:
     method: str
     objective_file_path: str
+
+@dataclass
+class FrameworkConfig:
+    target_information: TargetInformation
+    extract_information: ExtractInformation
+    llm: LLM
+    agents: Agents
+    orchestration: Orchestration
+    output: Output
+    evaluation: Evaluation
