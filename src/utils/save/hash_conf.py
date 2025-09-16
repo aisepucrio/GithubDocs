f57a44ff38402a0004d6f128208bbb494a53c9f1
PedroLopes <Pedro.Lopesverardo@gmail.com>
2025-09-16 09:25:12 -0300

    logging and hashing

---
+++ src/utils/save/hash_conf.py
@@ -0,0 +1,10 @@
+import yaml
+import json
+import hashlib
+
+def generate_config_hash(config_data: dict) -> str:
+    canonical_string = json.dumps(config_data, sort_keys=True, indent=None)
+
+    config_hash = hashlib.sha256(canonical_string.encode('utf-8')).hexdigest()
+
+    return config_hash
