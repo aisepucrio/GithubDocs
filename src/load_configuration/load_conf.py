20a468685ee4539c0cf3a61a448af1ed67856fc8
PedroLopes <Pedro.Lopesverardo@gmail.com>
2025-11-12 08:21:18 -0300

    simple extra line removal

--- src/load_configuration/load_conf.py
+++ src/load_configuration/load_conf.py
@@ -8,11 +8,6 @@ import os
 def read_config_file(file_data: str) -> dict[str, any]:
     return tomllib.loads(file_data)
 
-def render_prompt(template_path: str, prompt_file: str, variables: dict) -> str:
-    env = Environment(loader=FileSystemLoader(template_path))
-    template = env.get_template(prompt_file)
-    return template.render(variables)
-
 def load_config(file_path: str) -> BaseAppConfig:
     config = read_config_file(file_path)
 
@@ -22,7 +17,6 @@ def load_config(file_path: str) -> BaseAppConfig:
     orchestration_steps = []
 
     for step in config["agents"]["orchestration"]:
-        orch = Orchestration_step.model_validate(step)
         orchestration_steps.append(Orchestration_step.model_validate(step))
     
     sorted_steps = sorted(orchestration_steps, key=lambda x: x.step)
