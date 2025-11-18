7ed974468ce7bd5589e35591acab31e4b21e9473
PedroLopes <Pedro.Lopesverardo@gmail.com>
2025-11-18 12:37:21 -0300

    new gpt models add to LLM_CONTEXT_WINDOWS

--- src/llm_agent/context_window_size.py
+++ src/llm_agent/context_window_size.py
@@ -4,6 +4,9 @@ LLM_CONTEXT_WINDOWS = {
     "gemini-2.0-flash-lite": 1_048_576,
     "gemini-2.0-flash": 1_048_576,
     "gpt-5-2025-08-07": 400_000,
+    "gpt-5-mini": 400_000,
     "gpt-5-mini-2025-08-07": 400_000,
+    "gpt-5-nano": 400_000,
+    "gpt-5-nano-2025-08-07": 400_000,
     "mock": 10000000000000
 }
