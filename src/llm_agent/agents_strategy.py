8bd99f7e8cc0b50a9428dd242d27891903d34a9f
joao paulo <joaopaulosoareslopes22@gmail.com>
2026-01-12 15:13:05 -0300

    switched gemini to langchain

--- src/llm_agent/agents_strategy.py
+++ src/llm_agent/agents_strategy.py
@@ -5,35 +5,37 @@ import ollama
 
 from openai import OpenAI
 from google import genai
+from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.language_models.chat_models import BaseChatModel
 
 from .agent_interface import AIAgent
 
 class GeminiAgent(AIAgent):
     def __init__(self, model_name: str, api_key: str, base_prompt: str, temperature: float = 0.2):
         api_key = api_key or os.environ.get("GEMINI_API_KEY")
+                
         super().__init__(model_name, api_key, base_prompt, temperature)
-        self.client: genai.Client = genai.Client(api_key=self.api_key)
+
+        self.chat_model:BaseChatModel = init_chat_model("google_genai:" + model_name, api_key=api_key, temperature=temperature)
 
     def generate_response(self, input: str) -> str:
-        response = self.client.models.generate_content(
-            model=self.model_name, contents=self.base_prompt + "\n" + input,
-            config={"temperature": self.temperature}
+        response = self.chat_model.invoke(self.base_prompt + "\n" + input,
         )
-        self.output = response.text
+        self.output = response.content
         return self.output
     
-    def generate_response_with_prompt(self, prompt: str, input: str) -> str:
-        response = self.client.models.generate_content(
-            model=self.model_name, contents=prompt + "\n" + input,
-            config={"temperature": self.temperature}
+    def generate_response_with_prompt(self,  prompt: str, input: str) -> str:
+        response = self.chat_model.invoke(
+             prompt + "\n" + input,
         )
-        self.output = response.text
+        self.output = response.content
         return self.output
-
+    
     def _count_tokens(self, input: str) -> int:
-        return self.client.models.count_tokens(
-            model=self.model_name, contents=self.base_prompt + "\n" + input
-        ).total_tokens
+        return self.chat_model.get_num_tokens(
+         self.base_prompt + "\n" + input
+        )
 
 class GPTAgent(AIAgent):
     def __init__(self, model_name: str, api_key: str, base_prompt: str, temperature: float = 0.2):
